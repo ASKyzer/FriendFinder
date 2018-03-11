dfd3432f116c4e943689c1b7dd8d28c927344df8
Adrian Kyzer <adriankyzer@gmail.com>
2018-03-11 19:03:01 -0400

    got user input to post in api/friends., ran a nested loop to compare the user scores with friends scores.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,12 @@
 //* DATA *//
 // This will hold array of each person that takes the survey.
 // Fill with dummy arrays so we can run calculations in testing
+// This can also be left with empty values
 
 var friends = [
   {
-    name: "Adrian",
-    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    name : "Adrian",
+    photo: "https://cdn.pixabay.com/photo/2018/03/01/02/47/summer-3189602_1280.jpg",
     scores: [
     "4",
     "2",
@@ -21,7 +22,7 @@ var friends = [
   },
   {
     name: "Rob",
-    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    photo: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_1280.jpg",
     scores: [
     "1",
     "5",
@@ -37,7 +38,7 @@ var friends = [
   },
   {
     name: "Evy",
-    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    photo: "https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2563491_1280.jpg",
     scores: [
     "3",
     "3",
@@ -53,7 +54,7 @@ var friends = [
   },
   {
     name: "Shawn",
-    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    photo: "https://cdn.pixabay.com/photo/2018/03/09/06/45/nature-3210777_1280.jpg",
     scores: [
     "3",
     "4",
@@ -68,8 +69,8 @@ var friends = [
     ]
   },
   {
-    name: "adrian",
-    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    name: "Amber",
+    photo: "https://cdn.pixabay.com/photo/2018/01/06/09/25/hijab-3064633_1280.jpg",
     scores: [
     "5",
     "2",
@@ -82,7 +83,7 @@ var friends = [
     "2",
     "5"
     ]
-  },
+  }
 ]
 
 // exort the array so it's accessible to other files using required
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,19 +11,51 @@ module.exports = function(app) {
   // API GET Request
 
   app.get('/api/friends', function(req, res) {
+
     res.json(friends)
-  })
+
+  }) // nd of app.get
 
   // API POST Request
 
   app.post('/api/friends', function(req, res) {
 
+    var userData = req.body
+    var userName = userData.name
+    var userPhoto = userData.photo
+    var userScores = userData.scores
+
+    var friendsName;
+    var friendsPhoto;
+    var friendsScores;
+
+    var totalDifference = 0
+
+    var scoreDifferences = []
+
+
+    for (var i = 0; i < friends.length; i++) {
+
+      var totalDifference = 0
+
+      friendsName = friends[i].name
+      friendsPhoto = friends[i].photo
+      friendsScores = friends[i].scores
+
+      for (var j = 0; j < 10; j++) {
+
+        totalDifference += Math.abs(parseInt(friendsScores[j]) - parseInt(userScores[j]))
+
+        scoreDifferences.push(totalDifference)
+      } // end of for loop j
+
+    } // end of for loop i
+    console.log(totalDifference);
+    console.log(scoreDifferences);
+
     // push the survey into frendsData array
     friends.push(req.body)
-    res.json(true)
 
-    console.log(friends)
+  }) // end of app.post
 
-  })
-
-}
+} // end of module.exports function
