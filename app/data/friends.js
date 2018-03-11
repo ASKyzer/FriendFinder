cef2588d0a7d2e63fe5dbbdd9215012fad34ad84
Adrian Kyzer <adriankyzer@gmail.com>
2018-03-11 14:47:31 -0400

    added dummy friends array for testing calculations.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,89 @@
 //* DATA *//
 // This will hold array of each person that takes the survey.
+// Fill with dummy arrays so we can run calculations in testing
 
-var friendsArray = [
+var friends = [
   {
-    'name': '',
-    'photo': '',
-    'scores': []
-  }
+    name: "Adrian",
+    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    scores: [
+    "4",
+    "2",
+    "4",
+    "2",
+    "4",
+    "4",
+    "5",
+    "3",
+    "2",
+    "4"
+    ]
+  },
+  {
+    name: "Rob",
+    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    scores: [
+    "1",
+    "5",
+    "3",
+    "3",
+    "2",
+    "5",
+    "2",
+    "3",
+    "4",
+    "4"
+    ]
+  },
+  {
+    name: "Evy",
+    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    scores: [
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3"
+    ]
+  },
+  {
+    name: "Shawn",
+    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    scores: [
+    "3",
+    "4",
+    "3",
+    "4",
+    "3",
+    "4",
+    "3",
+    "4",
+    "3",
+    "4"
+    ]
+  },
+  {
+    name: "adrian",
+    photo: "https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander",
+    scores: [
+    "5",
+    "2",
+    "5",
+    "2",
+    "5",
+    "2",
+    "5",
+    "2",
+    "2",
+    "5"
+    ]
+  },
 ]
 
 // exort the array so it's accessible to other files using required
-module.exports = friendsArray
+module.exports = friends
