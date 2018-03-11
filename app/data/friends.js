ab630335b6090cbb867ba0747b28193d92f9fa3f
Adrian Kyzer <adriankyzer@gmail.com>
2018-03-11 11:59:18 -0400

    created the data array for each survey taker.

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+//* DATA *//
+// This will hold array of each person that takes the survey.
+
+var friends = [
+  {
+    'name': 'Adrian',
+    'photo': 'https://www.instagram.com/p/BZB1Xh7HczM/?taken-by=adriansander',
+    'scores': [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  }
+]
+
+// exort the array so it's accessible to other files using required
+module.exports = friends
